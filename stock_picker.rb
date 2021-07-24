16705f990ade25021e5b58775b3f30b4d511b31e
Tina Thompson <mark_junior13@yahoo.com>
2021-07-23 20:12:41 -0500

    finished

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,4 +1,4 @@
-def index_of_greatest_in_array(arr)
+def index_of_greatest_num_in_array(arr)
   arr.find_index(arr.max)
 end
 
@@ -14,17 +14,19 @@ end
 
 def best_sell_day_for_buy_day(prices, buy_day)
   profits = profits_of_each_sell_day_for_buy_day(prices,buy_day)
-  #print profits
-  #puts
-  best_sell_day = index_of_greatest_in_array(profits)
+  best_sell_day = index_of_greatest_num_in_array(profits)
   best_sell_day = buy_day if profits[best_sell_day] == 0
-  best_sell_day
+  {buy_day:buy_day, sell_day:best_sell_day, profit:profits[best_sell_day]}
 end
 
 def stock_picker(stock_prices)
-  best_sell_days = []
-  stock_prices.each_with_index do |index|
-    best_sell_days << best_sell_day_for_buy_day(stock_prices, index)
+  best_days = []
+  stock_prices.each_index do |buy_day|
+    best_days << best_sell_day_for_buy_day(stock_prices, buy_day)
   end
+  best = best_days.max_by {|day_info| day_info[:profit]}
+  [best[:buy_day], best[:sell_day]]
 end
 
+print stock_picker([17,3,6,9,15,8,6,1,10])
+puts
